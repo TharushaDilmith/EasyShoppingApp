2f05f084190b5d7d7a222f499c5436fab5e3cbeb
IT19153964 <tharushadilmith99@gmail.com>
2021-02-09 01:27:38 +0530

    First commit

---
+++ app/src/main/java/com/example/easyshopping/MyViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.easyshopping;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    TextView d_name,d_phone;
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        //gh
+        d_name = itemView.findViewById(R.id.d_name);
+        d_phone = itemView.findViewById(R.id.d_phone);
+    }
+}
